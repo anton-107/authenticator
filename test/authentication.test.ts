fe4329a467b94b8d5bee48cafeb3f4732992c0ac
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2022-05-21 22:26:17 +0200

    initial ts-jest setup

---
+++ test/authentication.test.ts
@@ -0,0 +1,7 @@
+import { Authenticator } from "../src/authenticator";
+
+describe('authentication', () => {
+  it('should give a jwt token to a signed in user', () => {
+    const a = new Authenticator();
+  });
+});
