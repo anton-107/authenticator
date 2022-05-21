6fa31466c25fbc39058b574dbc7b1529fb569973
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2022-05-21 22:50:04 +0200

    naive implementation and test suite (do not use in prod)

--- src/authenticator.ts
+++ src/authenticator.ts
@@ -1,3 +1,26 @@
+interface AuthenticationResult {
+  isAuthenticated: boolean;
+}
+
+export interface User {
+  username: string;
+  passwordHash: string;
+}
+
+export interface UserStore {
+  getUserByName(username: string): Promise<User | null>;
+}
+
+interface AuthenticatorProperties {
+  userStore: UserStore;
+}
+
 export class Authenticator {
-  
+  constructor(private props: AuthenticatorProperties) {}
+  public async authenticate(username: string, password: string): Promise<AuthenticationResult> {
+    const user = await this.props.userStore.getUserByName(username);
+    return {
+      isAuthenticated: user !== null && user.passwordHash === password
+    };
+  }
 }
--- test/authentication.test.ts
+++ test/authentication.test.ts
@@ -1,7 +1,37 @@
-import { Authenticator } from "../src/authenticator";
+import { Authenticator, User, UserStore } from "../src/authenticator";
+
+class TestUserStore implements UserStore {
+  public getUserByName(username: string): Promise<User | null> {
+    return Promise.resolve(this.getTestUser(username));
+  }
+  private getTestUser(username: string): User | null {
+    switch(username) {
+      case 'user1':
+        return {
+          username: 'user1',
+          passwordHash: '1234',
+        };
+      default:
+        return null;
+    }
+  }
+}
+const userStore = new TestUserStore();
 
 describe('authentication', () => {
-  it('should give a jwt token to a signed in user', () => {
-    const a = new Authenticator();
+  it('should not authenticate a user coming with a non-existent username', async () => {
+    const a = new Authenticator({userStore});
+    const user = await a.authenticate('user0', '1234');
+    expect(user.isAuthenticated).toBe(false);
+  });
+  it('should authenticate a user coming with a correct password', async () => {
+    const a = new Authenticator({userStore});
+    const user = await a.authenticate('user1', '1234');
+    expect(user.isAuthenticated).toBe(true);
+  });
+  it('should not authenticate a user coming with an incorrect password', async () => {
+    const a = new Authenticator({userStore});
+    const user = await a.authenticate('user1', '5678');
+    expect(user.isAuthenticated).toBe(false);
   });
 });
