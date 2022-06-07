a5800b339de825b18eee181857a11348939df7b7
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2022-06-07 22:43:31 +0200

    build scrypt-hashing into dist

---
+++ dist/scrypt-hashing.d.ts
@@ -0,0 +1,10 @@
+import { PasswordHashingFunction } from "./authenticator";
+export declare function handleError(
+  message: string,
+  error: Error | null,
+  reject: (message: string) => void
+): void;
+export declare class ScryptHashingFunction implements PasswordHashingFunction {
+  generateHash(password: string): Promise<string>;
+  verifyPasswordHash(password: string, passwordHash: string): Promise<boolean>;
+}
