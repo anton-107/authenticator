4077eaba1545aad884819af8900b4d4456beb2b6
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-02-02 20:04:54 +0100

    upgrade dependencies with vulnerabilities

--- dist/jwt-serializer.d.ts
+++ dist/jwt-serializer.d.ts
@@ -2,46 +2,28 @@ import { AuthTokensSerializer } from "./authenticator";
 import * as jwt from "jsonwebtoken";
 import { JwtPayload, Secret, SignCallback, SignOptions } from "jsonwebtoken";
 export interface JWT {
-  sign: (
-    payload: object,
-    secretOrPrivateKey: Secret,
-    options: SignOptions,
-    callback: SignCallback
-  ) => void;
-  verify: (
-    token: string,
-    secretOrPublicKey: Secret,
-    callback: jwt.VerifyCallback<JwtPayload | string>
-  ) => void;
+    sign: (payload: object, secretOrPrivateKey: Secret, options: SignOptions, callback: SignCallback) => void;
+    verify: (token: string, secretOrPublicKey: Secret, callback: jwt.VerifyCallback<JwtPayload | string>) => void;
 }
 export declare class StandardJwtImplementation implements JWT {
-  sign(
-    payload: object,
-    secretOrPrivateKey: Secret,
-    options: SignOptions,
-    callback: SignCallback
-  ): void;
-  verify(
-    token: string,
-    secretOrPublicKey: Secret,
-    callback: jwt.VerifyCallback<JwtPayload | string>
-  ): void;
+    sign(payload: object, secretOrPrivateKey: Secret, options: SignOptions, callback: SignCallback): void;
+    verify(token: string, secretOrPublicKey: Secret, callback: jwt.VerifyCallback<JwtPayload | string>): void;
 }
 export interface SecretKeyProvider {
-  getSecretKey(): Promise<string>;
+    getSecretKey(): Promise<string>;
 }
 export interface JWTSerializerProperties {
-  jwt: JWT;
-  secretKeyProvider: SecretKeyProvider;
+    jwt: JWT;
+    secretKeyProvider: SecretKeyProvider;
 }
 export declare class SimpleStringProvider implements SecretKeyProvider {
-  private secret;
-  constructor(secret: string);
-  getSecretKey(): Promise<string>;
+    private secret;
+    constructor(secret: string);
+    getSecretKey(): Promise<string>;
 }
 export declare class JWTSerializer implements AuthTokensSerializer {
-  private properties;
-  constructor(properties: JWTSerializerProperties);
-  generateAccessToken(username: string): Promise<string>;
-  decodeAccessToken(accessToken: string): Promise<string>;
+    private properties;
+    constructor(properties: JWTSerializerProperties);
+    generateAccessToken(username: string): Promise<string>;
+    decodeAccessToken(accessToken: string): Promise<string>;
 }
--- dist/scrypt-hashing.js
+++ dist/scrypt-hashing.js
@@ -3,39 +3,39 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ScryptHashingFunction = exports.handleError = void 0;
 const crypto_1 = require("crypto");
 function handleError(message, error, reject) {
-  if (error === null) {
-    return;
-  }
-  console.error(`Error: ${message}: ${error}`);
-  reject(`Error: ${message}: ${error}`);
+    if (error === null) {
+        return;
+    }
+    console.error(`Error: ${message}: ${error}`);
+    reject(`Error: ${message}: ${error}`);
 }
 exports.handleError = handleError;
 class ScryptHashingFunction {
-  generateHash(password) {
-    return new Promise((resolve, reject) => {
-      const salt = (0, crypto_1.randomBytes)(128).toString("hex");
-      (0, crypto_1.scrypt)(password, salt, 64, (err, key) => {
-        handleError("Error generating scrypt hash", err, reject);
-        resolve(`${salt}.${key.toString("hex")}`);
-      });
-    });
-  }
-  verifyPasswordHash(password, passwordHash) {
-    return new Promise((resolve, reject) => {
-      const parts = passwordHash.split(".");
-      if (parts.length !== 2) {
-        return reject(`Error getting salt from hash: ${passwordHash}`);
-      }
-      const salt = parts[0];
-      const hash = parts[1];
-      (0, crypto_1.scrypt)(password, salt, 64, (err, key) => {
-        handleError("Error generating scrypt hash", err, reject);
-        if (hash !== key.toString("hex")) {
-          return resolve(false);
-        }
-        resolve(true);
-      });
-    });
-  }
+    generateHash(password) {
+        return new Promise((resolve, reject) => {
+            const salt = (0, crypto_1.randomBytes)(128).toString("hex");
+            (0, crypto_1.scrypt)(password, salt, 64, (err, key) => {
+                handleError("Error generating scrypt hash", err, reject);
+                resolve(`${salt}.${key.toString("hex")}`);
+            });
+        });
+    }
+    verifyPasswordHash(password, passwordHash) {
+        return new Promise((resolve, reject) => {
+            const parts = passwordHash.split(".");
+            if (parts.length !== 2) {
+                return reject(`Error getting salt from hash: ${passwordHash}`);
+            }
+            const salt = parts[0];
+            const hash = parts[1];
+            (0, crypto_1.scrypt)(password, salt, 64, (err, key) => {
+                handleError("Error generating scrypt hash", err, reject);
+                if (hash !== key.toString("hex")) {
+                    return resolve(false);
+                }
+                resolve(true);
+            });
+        });
+    }
 }
 exports.ScryptHashingFunction = ScryptHashingFunction;
